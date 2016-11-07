25b2c5cf05c44c68a73afe52059e9b260c54e652
ESchulstad <eschulstad@gmail.com>
2016-11-06 22:30:04 -0500

    initial commit

---
+++ ContactInfo/Models/ContactModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ContactInfo.Models
+{
+    public class ContactModel
+    {
+        private string firstName;
+        private string lastName;
+        private string phoneNums;
+        private string email;
+
+        [Key]
+        public int RecordNumber {get; set;}
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set { this.firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return this.lastName; }
+            set { this.lastName = value; }
+        }
+
+        public string PhoneNums
+        {
+            get { return this.phoneNums; }
+            set { this.phoneNums = value; }
+        }
+
+        public string Email
+        {
+            get { return this.email; }
+            set { this.email = value; }
+        }
+
+
+    }
+}
